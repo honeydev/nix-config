89a9e980b50bbc048f175ae0e6b3b071ad896b79
honeydev <lebedev.alexey@protonmail.com>
2024-06-02 00:42:46 +1000

    init conf

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    # home-manager.url = "github:nix-community/home-manager";
+    home-manager = { url = "github:nix-community/home-manager/release-23.11";
+      # The `follows` keyword in inputs is used for inheritance.
+      # Here, `inputs.nixpkgs` of home-manager is kept consistent with
+      # the `inputs.nixpkgs` of the current flake,
+      # to avoid problems caused by different versions of nixpkgs.
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+    outputs = { self, nixpkgs, home-manager }: {
+    nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [ 
+      	./configuration.nix
+	 home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            # TODO replace ryan with your own username
+            home-manager.users.honey = import ./home.nix;
+            # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          }
+	];
+    };
+  };
+}
+
---
+++ xmonad/xmobarrc.hs
@@ -0,0 +1,61 @@
+
+  
+Config {
+       font = "xft:Zekton:size=13:bold:antialias=true"
+       , additionalFonts = [ "xft:FontAwesome:size=11" ]
+       , allDesktops = True
+       , bgColor = "#282c34"
+       , fgColor = "#bbc2cf"
+       , position = TopW L 100
+       , commands = [ Run Cpu [ "--template", "CPU: <total>%"
+                              , "--Low","3"
+                              , "--High","50"
+                              , "--low","#bbc2cf"
+                              , "--normal","#bbc2cf"
+                              , "--high","#fb4934"] 50
+
+                    , Run Memory ["-t","RAM: <usedratio>%"
+                                 ,"-H","80"
+                                 ,"-L","10"
+                                 ,"-l","#bbc2cf"
+                                 ,"-n","#bbc2cf"
+                                 ,"-h","#fb4934"] 50
+
+                    , Run Date "%a %b %_d %I:%M" "date" 300
+                    , Run DynNetwork ["-t","<fc=#4db5bd><fn=1></fn></fc> <rx>, <fc=#c678dd><fn=1></fn></fc> <tx>"
+                                     ,"-H","200"
+                                     ,"-L","10"
+                                     ,"-h","#bbc2cf"
+                                     ,"-l","#bbc2cf"
+                                     ,"-n","#bbc2cf"] 50
+
+                    , Run CoreTemp ["-t", "TEMP: <core0>°"
+                                   , "-L", "30"
+                                   , "-H", "75"
+                                   , "-l", "lightblue"
+                                   , "-n", "#bbc2cf"
+                                   , "-h", "#aa4450"] 50
+
+                    -- battery monitor
+                    , Run BatteryP       [ "BAT0" ]
+                                         [ "--template" , "<acstatus>"
+                                         , "--Low"      , "10"        -- units: %
+                                         , "--High"     , "80"        -- units: %
+                                         , "--low"      , "#fb4934" -- #ff5555
+                                         , "--normal"   , "#bbc2cf"
+                                         , "--high"     , "#98be65"
+
+                                         , "--" -- battery specific options
+                                                   -- discharging status
+                                                   , "-o"   , "<left>% (<timeleft>)"
+                                                   -- AC "on" status
+                                                   , "-O"   , "<left>% (<fc=#98be65>Charging</fc>)" -- 50fa7b
+                                                   -- charged status
+                                                   , "-i"   , "<fc=#98be65>Charged</fc>"
+                                         ] 50
+                    , Run StdinReader
+                    ]
+       , sepChar = "%"
+       , alignSep = "}{"
+       , template = "%StdinReader% }{ %cpu% | %coretemp% | %memory% | %battery% | %date%  |"   -- #69DFFA
+       }
