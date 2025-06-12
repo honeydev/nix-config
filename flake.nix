3904955a49684f62f1ffdcd4cda0cc3015cca72e
honeydev <lebedev.alexey@protonmail.com>
2025-06-12 17:29:27 +1000

    Up to 25.05.

--- flake.nix
+++ flake.nix
@@ -1,9 +1,9 @@
 {
   description = "A very basic flake";
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=release-24.11";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=release-25.05";
     # home-manager.url = "github:nix-community/home-manager";
-    home-manager = { url = "github:nix-community/home-manager/release-24.11";
+    home-manager = { url = "github:nix-community/home-manager/release-25.05";
       # The `follows` keyword in inputs is used for inheritance.
       # Here, `inputs.nixpkgs` of home-manager is kept consistent with
       # the `inputs.nixpkgs` of the current flake,
